e20766a2e9cbd882b73027582394a01cb90e84c1
Chenchal Subraveti <chenchals@gmail.com>
2017-04-18 13:25:02 -0500

    updated release notes

--- ReleaseNotes.m
+++ ReleaseNotes.m
@@ -1,4 +1,10 @@
+% Main PET processing workflow was developed in consultation with Chris in
+% Dr. Zald's lab. Chis has also been verifying all the processing steps in
+% this project.  
 % 15/03/2017 - Created release version preprocess_Fallypride_Scan1_Scan2
 % Normalizing code for processing both Scan1 and Scan2
+% /04/2017 - Created feature : generalize-pre-process for all PET studies
+% 14/04/2017 - Review with Linh
+% 18/04/2017 - Updated added creation of acqTimes files for dropped vols
 
 
